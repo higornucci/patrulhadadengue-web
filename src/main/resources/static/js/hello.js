e81bbed28b9775a0e577e7abf16e5c3c03ced640
Higor Nucci <higornucci@gmail.com>
2016-03-30 20:44:40 -0400

    botão para voltar para sua localização

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,5 +1,44 @@
 angular.module('dengoso', []).controller('MapCtrl', function ($scope, $http) {
 
+    function addYourLocationButton(map, initialLocation) {
+        var controlDiv = document.createElement('div');
+
+        var firstChild = document.createElement('button');
+        firstChild.style.backgroundColor = '#fff';
+        firstChild.style.border = 'none';
+        firstChild.style.outline = 'none';
+        firstChild.style.width = '28px';
+        firstChild.style.height = '28px';
+        firstChild.style.borderRadius = '2px';
+        firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+        firstChild.style.cursor = 'pointer';
+        firstChild.style.marginRight = '10px';
+        firstChild.style.padding = '0';
+        firstChild.title = 'Sua localização';
+        controlDiv.appendChild(firstChild);
+
+        var secondChild = document.createElement('div');
+        secondChild.style.margin = '5px';
+        secondChild.style.width = '18px';
+        secondChild.style.height = '18px';
+        secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)';
+        secondChild.style.backgroundSize = '180px 18px';
+        secondChild.style.backgroundPosition = '0 0';
+        secondChild.style.backgroundRepeat = 'no-repeat';
+        firstChild.appendChild(secondChild);
+
+        google.maps.event.addListener(map, 'center_changed', function () {
+            secondChild.style['background-position'] = '0 0';
+        });
+
+        firstChild.addEventListener('click', function () {
+            map.setCenter(initialLocation);
+        });
+
+        controlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+    }
+
     function adicionarMarcadorNoBanco(mapa, coordenadas) {
         var coordenadasParaEnvio = {
             latitude: coordenadas.lat,
@@ -55,6 +94,7 @@ angular.module('dengoso', []).controller('MapCtrl', function ($scope, $http) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 $scope.map.setCenter(initialLocation);
+                addYourLocationButton($scope.map, initialLocation);
             }, function () {
                 $scope.map.setCenter(campoGrande);
             });
